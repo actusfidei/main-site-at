5eef8b0df889b49e32e40bac36be212d8ce508b2
Andrew <actusfidei@outlook.com>
2020-07-17 23:36:48 +0300

    minor fixes v2

--- src/test/java/ru/geekbrains/main/site/at/SearchTest.java
+++ src/test/java/ru/geekbrains/main/site/at/SearchTest.java
@@ -8,12 +8,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SearchTest extends BaseTest {
     @Test
-    public void SearchTest() throws InterruptedException {
+    public void SearchCheck() throws InterruptedException {
 
 
         chromeDriver.findElement(By.cssSelector("button [class=\"svg-icon icon-popup-close-button \"]")).click();
         chromeDriver.findElement(By.cssSelector("[class=\"show-search-form\"] [class=\"svg-icon icon-search \"]")).click();
         chromeDriver.findElement(By.cssSelector("input[class=\"search-panel__search-field\"]")).sendKeys("java");
+        Thread.sleep(15000);
 
 
         int prof = Integer.parseInt(chromeDriver.findElement(By.xpath("html[1]/body[1]/div[1]/div[8]/div[1]/div[1]/div[1]/ul[1]/li[2]/a[1]/span[1]")).getText());
@@ -22,9 +23,9 @@ public class SearchTest extends BaseTest {
         int course = Integer.parseInt(chromeDriver.findElement(By.xpath("html[1]/body[1]/div[1]/div[8]/div[1]/div[1]/div[1]/ul[1]/li[3]/a[1]/span[1]")).getText());
         Assertions.assertTrue(course > 15);
 
-//        int webinar = Integer.parseInt(chromeDriver.findElement(By.xpath("html[1]/body[1]/div[1]/div[8]/div[1]/div[1]/div[1]/ul[1]/li[4]/a[1]/span[1]")).getText());
-//        Assertions.assertTrue(webinar >= 180 & webinar <= 300);
-//        Assertions.assertEquals(chromeDriver.findElement(By.xpath("(//a[contains(@class,'event__title h3')])[1]")).getText(), "Java Junior. Что нужно знать для успешного собеседования?");
+        int webinar = Integer.parseInt(chromeDriver.findElement(By.xpath("html[1]/body[1]/div[1]/div[8]/div[1]/div[1]/div[1]/ul[1]/li[4]/a[1]/span[1]")).getText());
+        Assertions.assertTrue(webinar >= 180 & webinar <= 300);
+        Assertions.assertEquals(chromeDriver.findElement(By.xpath("(//a[contains(@class,'event__title h3')])[1]")).getText(), "Java Junior. Что нужно знать для успешного собеседования?");
 
         int blog = Integer.parseInt(chromeDriver.findElement(By.xpath("html[1]/body[1]/div[1]/div[8]/div[1]/div[1]/div[1]/ul[1]/li[5]/a[1]/span[1]")).getText());
         Assertions.assertTrue(blog >= 300);
